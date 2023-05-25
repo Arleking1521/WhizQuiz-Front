2f3f018a5f23ae95cccdaf4392154e7ed36427b8
Arleking <Abdukarim600@gmail.com>
2023-05-26 02:10:55 +0600

    Created project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/App.css"
+import HomePage from "./pages/HomePage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, Router, Routes} from "react-router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import RulesPage from "./pages/RulesPage";
+import AboutPage from "./pages/AboutPage";
+import CommentsPage from "./pages/CommentsPage";
+import {BrowserRouter} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route exact path='/' element={<HomePage />} />
+                <Route exact path='/rules' element={<RulesPage />} />
+                <Route exact path='/comments' element={<CommentsPage />} />
+                <Route exact path='/about' element={<AboutPage />} />
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+
     </div>
   );
 }
